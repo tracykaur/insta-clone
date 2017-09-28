d52d816427363a88455ed43d944475c316ad5e10
tracykaur <tracykaur@yahoo.com>
2017-09-28 14:50:55 +1000

    added forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:image_url, :caption)
+      params.require(:post).permit(:image_url, :caption, :tag)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170928021947) do
+ActiveRecord::Schema.define(version: 20170928042800) do
 
   create_table "posts", force: :cascade do |t|
     t.string "image_url"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170928021947) do
     t.datetime "updated_at", null: false
     t.text "caption"
     t.integer "user_id"
+    t.string "tag"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -34,6 +35,7 @@ ActiveRecord::Schema.define(version: 20170928021947) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
